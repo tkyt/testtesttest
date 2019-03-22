432fadbaadd5edf2b6d18c35f422415d86f1c899
tkyt <cinnamonitylorx@gmail.com>
2019-03-22 23:59:51 +0900

    change theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -47,16 +47,15 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Google', 'http://google.com/'),
+         ('Apple', 'http://apple.com/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', '#'), ('Twitter', '$#'))
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
